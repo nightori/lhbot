7e75622b48a8a8f994b8abf9735491bb8751a9d9
Sergey Savin <nightori@yandex.ru>
2022-02-26 16:00:27 +0600

    Feature toggle for thread unarchiver

--- events/threadUpdate.js
+++ events/threadUpdate.js
@@ -1,8 +1,10 @@
+import cfg from './../config.js';
+
 export const name = 'threadUpdate';
 
 export function handler(oldThread, newThread) {
 	// automatically unarchive if it wasn't locked
-	if (newThread.archived && !newThread.locked) {
+	if (cfg.threadUnarchiverEnabled && newThread.archived && !newThread.locked) {
 		setTimeout(() => {
 			newThread.setArchived(false, 'Автоматическая разархивация');
 			console.log(`Unarchived thread: ${newThread.name}`);
